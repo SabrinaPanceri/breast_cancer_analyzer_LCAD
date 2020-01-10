0f990b06ba1cad145ffb14f810bc089e102ff2e0
Jean Pablo Vieira de Mello <jpvmello@hotmail.com>
2020-01-10 15:56:35 -0300

    Descompressão de imagens DICOM abrangendo todos os tipos de compressão

--- src/mammo_preprocessing/report_editor/anonymize.py
+++ src/mammo_preprocessing/report_editor/anonymize.py
@@ -31,36 +31,47 @@ if dicom_dir.endswith(os.sep):
     dicom_dir = dicom_dir[:-1]
 
 image_paths = Path(os.path.join(dicom_dir)).rglob('*.dcm')
-root_dir = dicom_dir.split(os.sep)[0]
+root_dir = dicom_dir.split(os.sep)[-1]
 out_dir = root_dir + '_anonymized'
 os.makedirs(out_dir, exist_ok = True)
 
+error = 0
+total = 0
 for image_path in image_paths:
-    image_path = str(image_path)
-    image_name = image_path.split(os.sep)[-1]
-    out_path = image_path.replace(root_dir, out_dir)
-    out_path_noimg = out_path.replace(image_name, '')
-    os.makedirs(out_path_noimg, exist_ok = True)
-    
-    print(image_name)
-    dcm_image = pydicom.dcmread(image_path)
-    dcm_image.decompress()
-    print('\tPatient name before:', dcm_image.PatientName)
-    if show:
-        plt.title('BEFORE')
-        plt.imshow(dcm_image.pixel_array, cmap = plt.cm.bone)
-        plt.show()
-    
-    dcm_image.PatientName = ''
-    pixel_data = dcm_image.pixel_array
-    cv2.rectangle(pixel_data, (x0, y0), (x1, y1), 0, cv2.FILLED)
-    dcm_image.PixelData = pixel_data.tobytes()
-    dcm_image.save_as(out_path)
-    print('\tSUCESS: Censure applied!')
-    
-    out_image = pydicom.dcmread(out_path)
-    print('\tPatient name after:', out_image.PatientName)
-    if show:
-        plt.title('AFTER')
-        plt.imshow(out_image.pixel_array, cmap = plt.cm.bone)
-        plt.show()
+	image_path = str(image_path)
+	image_name = image_path.split(os.sep)[-1]
+	out_path = image_path.replace(dicom_dir, out_dir)
+	out_path_noimg = out_path.replace(image_name, '')
+	os.makedirs(out_path_noimg, exist_ok = True)
+	
+	print(image_path)
+	total += 1
+	dcm_image = pydicom.dcmread(image_path)
+	try:
+		dcm_image.decompress()
+	except:
+		print('FAILED!')
+		error += 1
+		continue
+	print('\tPatient name before:', dcm_image.PatientName)
+	if show:
+		plt.title('BEFORE')
+		plt.imshow(dcm_image.pixel_array, cmap = plt.cm.bone)
+		plt.show()
+
+	dcm_image.PatientName = ''
+	pixel_data = dcm_image.pixel_array
+	cv2.rectangle(pixel_data, (x0, y0), (x1, y1), 0, cv2.FILLED)
+	dcm_image.PixelData = pixel_data.tobytes()
+	dcm_image.save_as(out_path)
+	print('\tSUCESS: Censure applied!')
+
+	out_image = pydicom.dcmread(out_path)
+	print('\tPatient name after:', out_image.PatientName)
+	if show:
+		plt.title('AFTER')
+		plt.imshow(out_image.pixel_array, cmap = plt.cm.bone)
+		plt.show()
+
+print('Failed:', error, '/', total)
+print(str(100*(1 - error/total)) + '%', 'successful!')
