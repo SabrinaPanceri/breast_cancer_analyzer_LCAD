2af13e7f431829805af20b152779c708a3bb3491
Raphael Vivacqua Carneiro <carneiro.raphael@gmail.com>
2020-01-22 04:33:58 -0300

    precision-recall

--- src/squeezeNet/metrics/metrics.gnuplot
+++
@@ -1,9 +0,0 @@
-set terminal png size 800,600; set output 'precision_recall_validation.png'; set xlabel 'Recall'; set ylabel 'Precision'; plot 'metrics_validation.txt' using 8:7 
-#pause -1
-set terminal png size 800,600; set output 'precision_recall_test.png'; set xlabel 'Recall'; set ylabel 'Precision'; plot 'metrics_test.txt' using 8:7 
-#pause -1
-set terminal png size 800,600; set output 'specificity_sensitivity_validation.png'; set xlabel 'Specificity'; set ylabel 'Sensitivity'; plot 'metrics_validation.txt' using 10:11
-#pause -1
-set terminal png size 800,600; set output 'specificity_sensitivity_test.png'; set xlabel 'Specificity'; set ylabel 'Sensitivity'; plot 'metrics_test.txt' using 10:11
-#pause -1
-
---
+++ src/squeezeNet/metrics/run.sh
@@ -0,0 +1,11 @@
+set -x
+python join_dataset_results.py   val_dataset.csv    val_probabilities.csv       -o val_results_squeezenet.csv
+python join_dataset_results.py   test_dataset.csv   test_probabilities.csv      -o test_results_squeezenet.csv
+python metrics.py   -i val_results_squeezenet.csv   -o val_metrics.txt          -r val_roc_curve.txt          -p val_pr_curve.txt  > val_log.txt
+python metrics.py   -i test_results_squeezenet.csv  -o test_metrics.txt         -r test_roc_curve.txt         -p test_pr_curve.txt        -tr val_roc_curve.txt         -tp val_pr_curve.txt  > test_log.txt
+python metrics.py   -i val_results_squeezenet.csv   -o val_metrics_0.0025.txt   -r val_roc_curve_0.0025.txt   -p val_pr_curve_0.0025.txt  -s 0.0025  > val_log_0.0025.txt
+python metrics.py   -i test_results_squeezenet.csv  -o test_metrics_0.0025.txt  -r test_roc_curve_0.0025.txt  -p test_pr_curve_0.0025.txt -tr val_roc_curve_0.0025.txt  -tp val_pr_curve_0.0025.txt  -s 0.0025  > test_log_0.0025.txt
+python metrics.py   -i val_results_squeezenet.csv   -o val_metrics_0.001.txt    -r val_roc_curve_0.001.txt    -p val_pr_curve_0.001.txt   -s 0.001   > val_log_0.001.txt
+python metrics.py   -i test_results_squeezenet.csv  -o test_metrics_0.001.txt   -r test_roc_curve_0.001.txt   -p test_pr_curve_0.001.txt  -tr val_roc_curve_0.001.txt   -tp val_pr_curve_0.001.txt   -s 0.001   > test_log_0.001.txt
+gnuplot run.gp
+set +x
