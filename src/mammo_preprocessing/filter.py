a3c3b35fe2f2c9473960e133f0eaa827f588e209
Gabriel Hendrix <hendrix.nove@gmail.com>
2020-05-20 20:42:20 -0300

    Alts

--- src/mammo_preprocessing/filter.py
+++ src/mammo_preprocessing/filter.py
@@ -1,12 +1,10 @@
 import cv2
 import os
 
-for root, dirs, files in os.walk('/Users/nove/breast_cancer_analyzer_LCAD/src/mammo_preprocessing/normalize'):
-    it = 0
+for root, dirs, files in os.walk('/home/wintermute/breast_cancer_analyzer_LCAD/dataset/Calc-CC_flipped_dataset/crops/BENIGN'):
     for i, f in enumerate(files):
         img = cv2.imread(root + '/' + f)
         imgAux = img[:, :img.shape[1], :]
         if ((imgAux == 0).sum() >= 50160):
-            cv2.imwrite(root + '/' +  'trash' + str(it), imgAux)
-            it+=1
+            cv2.imwrite(root + '/trash_files/' + f, imgAux); os.remove(root + '/' + f)
         del img
